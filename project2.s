e3afebabb8b0c24f8aa8da5bc67379b630f260ba
kaydrianne <33529955+kaydrianne@users.noreply.github.com>
2017-11-25 16:38:55 -0500

    Add files via upload

--- project2.s
+++ project2.s
@@ -3,7 +3,7 @@
 	error: .asciiz "Invalid hexadecimal number."	#Output to be printed on invalid input
 	newline: .asciiz "\n"							
 
-	
+
 .text
 main:
 	li $v0, 8				#Syscall for v0 = 8 is Read String
@@ -11,3 +11,16 @@ main:
 	la $a0, 0($t0)			#Loads Input into Argument
 	la $a1, 9				#Loads Length of Input
 	syscall 				#Calls syscall 8 - Read String
+
+	addi $t7, $t0, 8		#Move 9th byte of Input to Register
+	addi $s5, $t0, 0		#Move input to Register
+	add $s3, $zero, $zero   #Intialize Register to Zero
+
+length_of_input:				#Count length of Input
+	lb $t1, 0($s5)
+	beq $t1, 0, revert
+	beq $t1, 10, revert
+	addi $s3, $s3, 4
+	addi $s5, $s5, 1 
+	j length_of_input
+
